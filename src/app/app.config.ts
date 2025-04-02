f3071cc97acea4bb50d5e8a6fdf93ef4adbffbf7
PedroV <a_pedro.vigara.haro@iespablopiccaso.es>
2025-04-02 10:17:47 +0200

    Proyecto base

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,13 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { getMessaging, provideMessaging } from '@angular/fire/messaging';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay()), provideFirebaseApp(() => initializeApp({ projectId: "teamup-fd251", appId: "1:385199238281:web:560e0d15cbe0b24498aa58", storageBucket: "teamup-fd251.firebasestorage.app", apiKey: "AIzaSyBsqA6JdyYCzp1AlCHjQhbt3_v7JiVXXW0", authDomain: "teamup-fd251.firebaseapp.com", messagingSenderId: "385199238281", measurementId: "G-CWZB81KG7B" })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideDatabase(() => getDatabase()), provideMessaging(() => getMessaging()), provideStorage(() => getStorage())]
 };
---
+++ src/app/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBsqA6JdyYCzp1AlCHjQhbt3_v7JiVXXW0",
+    authDomain: "teamup-fd251.firebaseapp.com",
+    projectId: "teamup-fd251",
+    storageBucket: "teamup-fd251.firebasestorage.app",
+    messagingSenderId: "385199238281",
+    appId: "1:385199238281:web:560e0d15cbe0b24498aa58",
+    measurementId: "G-CWZB81KG7B"
+  }
+};
